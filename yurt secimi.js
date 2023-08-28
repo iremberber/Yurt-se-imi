9049d4bae1dfca0296ec3c4f40c9d079818fa4c1
iremberber <iremberber0@gmail.com>
2023-08-28 21:53:57 +0300

    Sorulara katsayı vermeye çalışıyorrum

--- yurt secimi.js
+++ yurt secimi.js
@@ -1,29 +1,32 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const startButton = document.getElementById("startButton");
-    const tableContainer = document.getElementById("tableContainer");
-    const tableBody = document.getElementById("tableBody");
-    const resultButton = document.getElementById("resultButton");
-    const resultText = document.getElementById("resultText");
-  
-    startButton.addEventListener("click", function () {
-      tableContainer.classList.remove("hidden");
-      resultButton.classList.remove("hidden");
-      startButton.style.display = "none";
-    });
-  
-    resultButton.addEventListener("click", function () {
-      let yesCount = 0;
-      const rows = tableBody.getElementsByTagName("tr");
-      for (const row of rows) {
-        const radioYes = row.querySelector('input[value="evet"]');
-        if (radioYes.checked) {
-          yesCount++;
-        }
-      }
-      const resultMessage = yesCount > rows.length / 2 ? "uygun :)" : "uygun değil :(";
-      resultText.textContent = `Toplam ${yesCount} soruya evet dedin. Bizce bu yurt senin için ${resultMessage}`;
-      resultText.classList.remove("hidden");
-      tableContainer.classList.add("hidden");
-    
-    });
+  const startButton = document.getElementById("startButton");
+  const tableContainer = document.getElementById("tableContainer");
+  const tableBody = document.getElementById("tableBody");
+  const resultButton = document.getElementById("resultButton");
+  const resultText = document.getElementById("resultText");
+  const body = document.getElementById("body");
+  const threshold = 50;
+
+  startButton.addEventListener("click", function () {
+    body.style.backgroundRepeat = "repeat";
+    tableContainer.classList.remove("hidden");
+    resultButton.classList.remove("hidden");
+    startButton.style.display = "none";
   });
+
+  resultButton.addEventListener("click", function () {
+    resultButton.classList.add("hidden");
+    let totalScore = 0;
+    const radios = document.querySelectorAll(".evet-hayir input[type='radio']:checked");
+
+    for (const radio of radios) {
+      const katsayi = parseInt(radio.getAttribute("data-katsayi"));
+      totalScore += katsayi;
+    }
+
+    const resultMessage = totalScore > threshold ? "uygun :)" : "uygun değil :(";
+    resultText.textContent = `Toplam puan: ${totalScore}. Bizce bu yurt senin için ${resultMessage}`;
+    resultText.classList.remove("hidden");
+    tableContainer.classList.add("hidden");
+  });
+});
